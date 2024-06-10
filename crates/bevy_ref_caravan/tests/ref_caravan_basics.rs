7c85b2b03239fc4ecaf8082d2b692a0e889a644e
Dominic Rooney <orangutanrider@gmail.com>
2024-06-10 18:04:35 +0100

    Test successful

--- crates/bevy_ref_caravan/tests/ref_caravan_basics.rs
+++ crates/bevy_ref_caravan/tests/ref_caravan_basics.rs
@@ -1,4 +1,4 @@
-use bevy_ecs::prelude::*;
+use bevy_ecs::{prelude::*, schedule::ScheduleLabel};
 use bevy_ref_caravan::ref_caravan;
 
 #[derive(Component)]
@@ -12,22 +12,35 @@ impl ToOranges {
 #[derive(Component)]
 struct Oranges(u32);
 
+#[derive(ScheduleLabel, Clone, Debug, PartialEq, Eq, Hash)]
+struct TestSchedule;
+
 #[test]
 fn ref_caravan_basics() {
     let mut world =  World::new();
     
-    let destination = world.spawn(Oranges(3)).id();
+    // Create entities + components
+    let destination = world.spawn(Oranges(0)).id(); // No oranges
     world.spawn(ToOranges(destination)); // Origin
 
+    // Create system
+    let mut schedule = Schedule::new(TestSchedule);
+    schedule.add_systems(one_orange_sys);
+    schedule.initialize(&mut world).unwrap();
+    schedule.run(&mut world); // Run system
 
+    // Get edited entity, and assert
+    let oranges = world.entity(destination).get::<Oranges>().unwrap();
+    assert!(oranges.0 == 1);
 }
 
-fn to_oranges_sys(
+fn one_orange_sys(
     origin_q: Query<&ToOranges>,
     mut dest_q: Query<&mut Oranges>,
 ) {
     for to_oranges in origin_q.iter() {
         ref_caravan!(to_oranges :: dest_q = mut oranges;);
 
+        oranges.0 = 1;
     }
 }
