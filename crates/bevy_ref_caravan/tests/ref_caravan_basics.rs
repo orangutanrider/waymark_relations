1e16179b5d1619b4f9c6decc94ce710321711c07
Dominic Rooney <orangutanrider@gmail.com>
2024-06-10 15:51:04 +0100

    Basics test (fail)
    There is no mutation detection during bindings.
    Also the entity to bindings seperator "\\", it seems that there is either something broken there or that syntax is treated specially.
    
    Given that the macro isn't expanding at all, there is an error happening somewhere, leading me to think that there is something up with "\\" as special symbols.

---
+++ crates/bevy_ref_caravan/tests/ref_caravan_basics.rs
@@ -0,0 +1,33 @@
+use bevy_ecs::prelude::*;
+use bevy_ref_caravan::ref_caravan;
+
+#[derive(Component)]
+struct ToOranges(Entity);
+impl ToOranges {
+    fn go(&self) -> Entity {
+        return self.0
+    }
+}
+
+#[derive(Component)]
+struct Oranges(u32);
+
+#[test]
+fn ref_caravan_basics() {
+    let mut world =  World::new();
+    
+    let destination = world.spawn(Oranges(3)).id();
+    world.spawn(ToOranges(destination)); // Origin
+
+
+}
+
+fn to_oranges_sys(
+    origin_q: Query<&ToOranges>,
+    mut dest_q: Query<&mut Oranges>,
+) {
+    for to_oranges in origin_q.iter() {
+        ref_caravan!(to_oranges \\ dest_q = mut oranges;);
+
+    }
+}
