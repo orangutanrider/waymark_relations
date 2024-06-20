62d196b7076a65322874d7f81365b248ac7cfd05
Dominic Rooney <orangutanrider@gmail.com>
2024-06-20 13:20:08 +0100

    Problem
    The conditional compilation for test doesn't work for an external tests folder.
    This is a problem, because I cannot use an internal tests folder; the macro cannot be called within its own library as a macro.
    I suppose, I just have to flag it as a debug assertion, and that will be fine.

---
+++ crates/bevy_ref_caravan/tests/ref_caravan_basics.rs
@@ -0,0 +1,65 @@
+use bevy_ref_caravan::*;
+
+#[test]
+fn basic_ref_caravan() {
+    assert_ref_caravan!((
+        to_oranges :: oranges_q = oranges;
+    ) (
+        let Ok(oranges) = oranges_q.get(to_oranges.go()) else {
+            continue;
+        };
+    ));
+}
+
+#[test]
+fn no_break_ref_caravan() {
+    assert_ref_caravan!((
+        to_oranges :: oranges_q = oranges
+    ) (
+        let Ok(oranges) = oranges_q.get(to_oranges.go()) else {
+            continue;
+        };
+    ));
+}
+
+#[test]
+fn multi_ref_caravan() {
+    assert_ref_caravan!((
+        to_oranges :: oranges_q = oranges;
+        to_apples :: apples_q = apples;
+    ) (
+        let Ok(oranges) = oranges_q.get(to_oranges.go()) else {
+            continue;
+        };
+
+        let Ok(apples) = apples_q.get(to_apples.go()) else {
+            continue;
+        };
+    ));
+}
+
+#[test]
+fn mut_ref_caravan() {
+    assert_ref_caravan!((
+        to_oranges :: oranges_q = mut oranges;
+    ) (
+        let Ok(mut oranges) = oranges_q.get_mut(to_oranges.go()) else {
+            continue;
+        };
+    ));
+}
+
+#[test]
+fn into_next_ref_caravan() {
+    assert_ref_caravan!((
+        to_hub :: hub_q = to_oranges => to_oranges :: oranges_q = oranges;
+    ) (
+        let Ok(to_oranges) = hub_q.get(to_hub.go()) else {
+            continue;
+        };
+
+        let Ok(oranges) = oranges_q.get(to_oranges.go()) else {
+            continue;
+        };
+    ));
+}
