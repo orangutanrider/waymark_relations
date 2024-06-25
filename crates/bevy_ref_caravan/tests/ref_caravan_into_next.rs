83aa40e3e2c4eb9e60afd6e789df20029ddfc67a
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-06-25 13:34:54 +0100

    Nesting test fail
    The actual output is as follows:
    let Ok((to_oranges,to_apples)) = hub_q.get(to_hub.go())else {
        continue;
    };
    let Ok(oranges,apples_q = apples,) = oranges_q.get(to_oranges.go())else {
        continue;
    };
    
    Seemingly something is broken with the individual bindings collection function. However, it also didn't seem to enter into the 3rd query step at all.

--- crates/bevy_ref_caravan/tests/ref_caravan_into_next.rs
+++ crates/bevy_ref_caravan/tests/ref_caravan_into_next.rs
@@ -13,3 +13,23 @@ fn basic_into_next_ref_caravan() {
         };
     ));
 }
+
+#[test]
+fn nested_ref_caravan() {
+    assert_ref_caravan!((
+        to_hub :: hub_q = (to_oranges, to_apples) => {
+            oranges_q = oranges,
+            apples_q = apples,
+        }
+    ) (
+        let Ok((to_oranges, to_apples)) = hub_q.get(to_hub.go()) else {
+            continue;
+        };
+        let Ok(oranges) = oranges_q.get(to_oranges.go()) else {
+            continue;
+        };
+        let Ok(apples) = apples_q.get(to_apples.go()) else {
+            continue;
+        };
+    ));
+}
