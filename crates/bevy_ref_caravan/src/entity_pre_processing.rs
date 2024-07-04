cd817da84719e3eb11343314e901238889fd8f0e
Dominic Rooney <orangutanrider@gmail.com>
2024-07-04 17:03:38 +0100

    Code construction

--- crates/bevy_ref_caravan/src/entity_pre_processing.rs
+++ crates/bevy_ref_caravan/src/entity_pre_processing.rs
@@ -1,14 +1,17 @@
+use core::panic;
+use std::str::FromStr;
+
 use proc_macro::*;
 use proc_macro::token_stream::IntoIter as TokenIter;
 
 use crate::syntax_in::{ENTITY_PRE_PROCESS_NOTATION, ENTITY_PRE_PROCESS_VAR};
 
 pub(crate) struct EntityPreProcess {
-    pub(crate) farm: TokenStream,
-    pub(crate) suffix: Ident,
+    pub(crate) farm: TokenStream, // The origin of constructed pre-processing statements; When one is created the farm is read to do so.
+    pub(crate) suffix: String,
 } 
 impl EntityPreProcess {
-    pub(crate) fn create_pre_processing_code(&self, entity_clause: &Vec<TokenTree>) -> TokenStream {
+    pub(crate) fn create_pre_processing_code(&self, entity_clause: TokenStream) -> TokenStream {
         // PSEUDOCODE
         // Iterate across farm and construct a seperate and new one.
         // When notation is found, create suffix-ed entity clause.
@@ -16,8 +19,16 @@ impl EntityPreProcess {
         // Return this altered copy of farm.
 
         let iter = self.farm.clone().into_iter();
+        let suffixed_entity_clause = entity_clause.to_string() + &self.suffix;
+        let Ok(suffixed_entity_clause) = TokenStream::from_str(&suffixed_entity_clause) else {
+            panic!("Unexpected lex error, while creating a token stream, from a suffixed entity clause, during the construction of entity pre-processing code.")
+        };
 
-        todo!()
+        let mut output = Vec::new(); 
+        build_pre_processing_code(iter, &entity_clause, &suffixed_entity_clause, &mut output);
+        let output = TokenStream::from_iter(output.into_iter());
+
+        return output;
     }
 }
 
@@ -43,13 +54,13 @@ fn build_pre_processing_code(
             return build_pre_processing_code(iter, entity_clause, suffixed_entity_clause, output); // Continue
         },
         TokenTree::Punct(punct) => { // Check for relevant symbols and insert relevant data into output when found
-            if punct == ENTITY_PRE_PROCESS_NOTATION {
+            if punct == ENTITY_PRE_PROCESS_NOTATION { // Add suffixed entity clause
                 let token_push = Group::new(Delimiter::None, suffixed_entity_clause.clone());
                 output.push(TokenTree::Group(token_push));
                 return build_pre_processing_code(iter, entity_clause, suffixed_entity_clause, output);
             }
 
-            if punct == ENTITY_PRE_PROCESS_VAR {
+            if punct == ENTITY_PRE_PROCESS_VAR { // Add entity clause
                 let token_push = Group::new(Delimiter::None, entity_clause.clone());
                 output.push(TokenTree::Group(token_push));
                 return build_pre_processing_code(iter, entity_clause, suffixed_entity_clause, output);
