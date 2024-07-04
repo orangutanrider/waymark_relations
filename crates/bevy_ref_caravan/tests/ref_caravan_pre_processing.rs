0ce56d154b8b945e1e90dc506cb067290e2b7f32
Dominic Rooney <orangutanrider@gmail.com>
2024-07-04 19:29:49 +0100

    Bug fixed
    The problem wasn't complex; It is that pound signs aren't allowed, not an ASCII character or something.
    Test passing.

--- crates/bevy_ref_caravan/tests/ref_caravan_pre_processing.rs
+++ crates/bevy_ref_caravan/tests/ref_caravan_pre_processing.rs
@@ -2,23 +2,20 @@ use bevy_ref_caravan::*;
 
 #[test]
 fn pre_processing_test() {
-    ref_caravan!(
-        £ peeled {
-            let £ = Some(%) else {
-                continue;
-            }
-        }
-        oranges :: oranges_q = oranges;
-    )
-
     assert_ref_caravan!((
-        £ peeled {
-            let £ = Some(%) else {
+        $ _peeled {
+            let $ = Some(%) else {
                 continue;
-            }
+            };
         }
+
         oranges :: oranges_q = oranges;
     ) (
-
+        let oranges_peeled = Some(oranges) else {
+            continue;
+        };
+        let Ok(oranges) = oranges_q.get(oranges_peeled) else {
+            continue;
+        };
     ));
 }
